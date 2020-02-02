cd04f661c3a39d1eb9a4e46b095dbf3bf698ac69
Emanuel H. Farias <emanuelfarias@outlook.com>
2020-02-02 15:52:01 -0300

    add ability to update Recipient

--- src/app/controllers/RecipientController.js
+++ src/app/controllers/RecipientController.js
@@ -5,6 +5,18 @@ class RecipientController {
     const recipient = await Recipient.create(req.body);
     return res.status(201).json(recipient);
   }
+
+  async update(req, res) {
+    const { id } = req.params;
+
+    const recipient = await Recipient.findByPk(id);
+    if (!recipient) {
+      return res.status(404).json('Destinatário não encontrado.');
+    }
+
+    await recipient.update(req.body);
+    return res.status(201).json(req.body);
+  }
 }
 
 export default new RecipientController();
--- src/routes.js
+++ src/routes.js
@@ -6,5 +6,6 @@ const routes = new Router();
 
 routes.post('/session', SessionController.store);
 routes.post('/recipient', RecipientController.store);
+routes.put('/recipient/:id', RecipientController.update);
 
 export default routes;
