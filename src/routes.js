60e3ddd47ad8291779264332453025ab5857cd45
Emanuel H. Farias <emanuelfarias@outlook.com>
2020-02-02 16:04:13 -0300

    adicioan e modifica destinatarios apenas usuarios autenticados

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 import SessionController from './app/controllers/SessionController';
 import RecipientController from './app/controllers/RecipientController';
+import AuthMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
 routes.post('/session', SessionController.store);
+
+routes.use(AuthMiddleware);
 routes.post('/recipient', RecipientController.store);
 routes.put('/recipient/:id', RecipientController.update);
 
