4a4c9c693ed3f9308bf0d72bd3a9bd08deb8d2bd
Emanuel H. Farias <emanuelfarias@outlook.com>
2020-02-02 14:59:46 -0300

    add validation with Yup

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,12 +1,24 @@
+import * as yup from 'yup';
 import jwt from 'jsonwebtoken';
 import User from '../models/User';
 import authConfig from '../../config/auth';
 
 class SessionController {
   async store(req, res) {
+    const schema = yup.object().shape({
+      email: yup
+        .string()
+        .email()
+        .required(),
+      password: yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'validation fails.' });
+    }
+
     const { email, password } = req.body;
     const user = await User.findOne({ where: { email } });
-
     if (!user) {
       return res.status(401).json({ error: 'User not found.' });
     }
