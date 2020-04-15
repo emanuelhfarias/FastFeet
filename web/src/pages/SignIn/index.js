b5b8642f4d0fa39dc41a5fe803bef13aa5785c51
Emanuel H. Farias <emanuelfarias@outlook.com>
2020-04-14 21:46:00 -0300

    add loading in SignIn button during auth

--- web/src/pages/SignIn/index.js
+++ web/src/pages/SignIn/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import * as Yup from 'yup';
 
 import { Wrapper, Form, Input } from './styles';
@@ -17,6 +17,7 @@ const schema = Yup.object().shape({
 
 export default function SignUp() {
   const dispatch = useDispatch();
+  const loading = useSelector((state) => state.auth.loading);
 
   function handleSubmit({ email, password }) {
     dispatch(signInRequest(email, password));
@@ -51,7 +52,9 @@ export default function SignUp() {
           </label>
         </div>
 
-        <button type="submit">Entrar no sistema</button>
+        <button type="submit">
+          {loading ? 'Carregando...' : 'Entrar no sistema'}
+        </button>
       </Form>
     </Wrapper>
   );
