4ac0e0b49188850909d492b0f94a61c07b72bc68
Emanuel H. Farias <emanuelfarias@outlook.com>
2020-04-14 21:53:57 -0300

    send token JWT in all requests

--- web/src/pages/Deliveries/index.js
+++ web/src/pages/Deliveries/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 
+import api from '../../services/api';
+
 export default function Deliveries() {
+  api.get('/delivery'); // testing token present in header request
   return <h1>Deliveries</h1>;
 }
