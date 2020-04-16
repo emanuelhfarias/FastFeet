1325d6c8682efd57d246086ab4dc3016aaad5908
Emanuel H. Farias <emanuelfarias@outlook.com>
2020-04-15 22:52:05 -0300

    stylize deliveries page

--- web/src/pages/Deliveries/index.js
+++ web/src/pages/Deliveries/index.js
@@ -1,8 +1,55 @@
 import React from 'react';
 
-import api from '../../services/api';
+import {
+  Content,
+  Title,
+  Actions,
+  SearchBox,
+  Button,
+  Table,
+} from '../_layouts/default/styles';
 
 export default function Deliveries() {
-  api.get('/delivery'); // testing token present in header request
-  return <h1>Deliveries</h1>;
+  return (
+    <Content>
+      <Title>Gerenciando Encomendas</Title>
+
+      <Actions>
+        <SearchBox placeholder="Buscar por encomendas" />
+        <Button>Cadastrar</Button>
+      </Actions>
+
+      <Table>
+        <thead>
+          <th>ID</th>
+          <th>Destinatário</th>
+          <th>Entregador</th>
+          <th>Cidade</th>
+          <th>Estado</th>
+          <th>Status</th>
+          <th>Ações</th>
+        </thead>
+        <tbody>
+          <tr>
+            <td>#01</td>
+            <td>Ludwig van Beethoven</td>
+            <td>John Doe</td>
+            <td>Rio do Sul</td>
+            <td>Santa Catarina</td>
+            <td>Entregue</td>
+            <td>...</td>
+          </tr>
+          <tr>
+            <td>#02</td>
+            <td>Wolfgang Amadeus</td>
+            <td>Manolo</td>
+            <td>Florianópolis</td>
+            <td>Santa Catarina</td>
+            <td>Pendente</td>
+            <td>...</td>
+          </tr>
+        </tbody>
+      </Table>
+    </Content>
+  );
 }
--- web/src/pages/_layouts/default/styles.js
+++ web/src/pages/_layouts/default/styles.js
@@ -4,3 +4,61 @@ export const Wrapper = styled.div`
   height: 100%;
   background: #f5f5f5;
 `;
+
+export const Content = styled.div`
+  max-width: 1000px;
+  margin: 0 auto;
+`;
+
+export const Actions = styled.div`
+  padding: 20px 0;
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const Title = styled.h1`
+  margin-top: 25px;
+  font-size: 24px;
+  color: #444;
+`;
+
+export const SearchBox = styled.input`
+  height: 36px;
+
+  ::placeholder {
+    color: #666;
+  }
+`;
+
+export const Table = styled.table`
+  width: 100%;
+  border-spacing: 0 20px;
+
+  thead {
+    color: #444;
+    font-size: 16px;
+  }
+
+  td {
+    font-size: 16px;
+    color: #666;
+    background: #fff;
+    height: 57px;
+    text-align: center;
+  }
+`;
+
+export const Button = styled.button.attrs({
+  type: 'button',
+})`
+  background: #7d40e7;
+  border-radius: 4px;
+  border: 0px;
+  width: 142px;
+  height: 36px;
+  color: #fff;
+  font-weight: bold;
+  text-transform: uppercase;
+`;
+
+export const BackButton = styled.div``;
