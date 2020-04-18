0fcc9aaf9e7b4982a5d6b55746128d29057aab92
Emanuel H. Farias <emanuelfarias@outlook.com>
2020-04-17 22:41:41 -0300

    fetch and list problems

--- web/src/pages/Problems/index.js
+++ web/src/pages/Problems/index.js
@@ -1,5 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import { Content, Title, Table } from '../_layouts/default/styles';
+
+import api from '../../services/api';
 
 export default function Problems() {
-  return <h1>Problems</h1>;
+  const [problems, setProblems] = useState([]);
+
+  useEffect(() => {
+    async function fetchProblems() {
+      const response = await api.get('/delivery/problems');
+      setProblems(response.data);
+    }
+
+    fetchProblems();
+  }, []);
+
+  return (
+    <Content>
+      <Title>Problemas nas Entregas</Title>
+
+      <Table>
+        <thead>
+          <tr>
+            <th>Encomenda</th>
+            <th>Problema</th>
+            <th>Ações</th>
+          </tr>
+        </thead>
+        <tbody>
+          {problems.map((problem) => (
+            <tr key={problem.id}>
+              <td>#{problem.delivery_id}</td>
+              <td>{problem.description}</td>
+              <td>...</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </Content>
+  );
 }
