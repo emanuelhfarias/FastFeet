5b21e374f3ed8285b3a10d7997c6df9674e3410d
Emanuel H. Farias <emanuelfarias@outlook.com>
2020-06-21 21:43:11 -0300

    add default background
    $ yarn add styled-components

--- mobile/src/pages/Main/index.js
+++ mobile/src/pages/Main/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Text } from 'react-native';
+
+import Background from '../../components/Background';
 
 export default function Main() {
-  return <View />;
+  return (
+    <Background>
+      <Text>Sign In</Text>
+    </Background>
+  );
 }
 
 Main.navigationOptions = {
